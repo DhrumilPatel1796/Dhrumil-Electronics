69afb1e75d7d2d5c8f1f6f63b7853ca6d81f6eb2
Dhrumil <dhrumilcsk@gmail.com>
2022-02-10 21:14:51 -0500

    Change content to be copyright only

--- themes/projdeliverablesone/footer.php
+++ themes/projdeliverablesone/footer.php
@@ -13,17 +13,10 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'projdeliverablesone' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'projdeliverablesone' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'projdeliverablesone' ), 'projdeliverablesone', '<a href="https://pateldhrumil.com">Dhrumil Patel</a>' );
-				?>
+			<?php
+			/* translators: 1: Theme name, 2: Theme author. */
+			printf( esc_html__( 'Copyright: %1$s by %2$s.', 'projdeliverablesone' ), '2022', '<a href="https://pateldhrumil.com">Dhrumil Patel</a>' );
+			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
