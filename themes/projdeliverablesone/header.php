e9e60d7ca8dbe02d47b73d483f18702461bb8063
Dhrumil <dhrumilcsk@gmail.com>
2022-02-18 21:10:21 -0500

    Custom Header Created

--- themes/projdeliverablesone/header.php
+++ themes/projdeliverablesone/header.php
@@ -25,32 +25,104 @@
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			
 			<?php
-			the_custom_logo();
+				the_custom_logo();
+
+				// <button>My Account</button>
+			?>
+			<!-- <img src="./assets/img/logo.png" /> -->
+			<?php
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title">
+					<a href="
+						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					</a>
+				</h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title">
+					<a href="
+						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><button>My Account</button>
+					</a>
+				</p>
 				<?php
 			endif;
-			$projdeliverablesone_description = get_bloginfo( 'description', 'display' );
-			if ( $projdeliverablesone_description || is_customize_preview() ) :
+			?>
+			<?php
+			if ( is_front_page() && is_home() ) :
 				?>
-				<p class="site-description"><?php echo $projdeliverablesone_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+				<h1 class="site-title">
+					<a href="
+						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					</a>
+				</h1>
+				<?php
+			else :
+				?>
+				<p class="site-login">
+					<a href="
+						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><button>Login</button>
+					</a>
+				</p>
+				<?php
+			endif;
+			?>
+			<?php
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="search">
+					<a href="
+						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					</a>
+				</h1>
+				<?php
+			else :
+				?>
+				<p class="site-search">
+					<input type="search
+						<?php echo esc_url( home_url( '/' ) ); ?>" placeholder="Search" rel="home">
+				</p>
+				<?php
+			endif;
+			?>
+			</div>
+			<div class="nav_menu">
+			<?php
+			if ( is_front_page() && is_home() ) :
+				?>
+				<?php
+			else :
+				?>
+				<p class="menu">
+					<a href="
+						<?php echo esc_url( home_url( '/home' ) ); ?>" rel="home"><h3>Home</h3>
+					</a>
+					<a href="
+						<?php echo esc_url( home_url( '/about' ) ); ?>" rel="about"><h3>About</h3>
+					</a>
+					<a href="
+						<?php echo esc_url( home_url( '/block' ) ); ?>" rel="blog"><h3>Block Test</h3>
+					</a>
+					<a href="
+						<?php echo esc_url( home_url( '/contact' ) ); ?>" rel="contact"><h3>Contact</h3>
+					</a>
+				</p>
+				<?php
+			endif;
+			?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<!-- <nav id="site-navigation" class="main-navigation">
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'menu-primary',
+					'theme_location' => 'primary-menu',
 					'menu_id'        => 'primary-menu',
 				)
 			);
 			?>
-		</nav><!-- #site-navigation -->
+		</nav> #site-navigation -->
 	</header><!-- #masthead -->
