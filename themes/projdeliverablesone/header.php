dceba39def1ac983c35e89d7f2dfd07bfd9c8581
Dhrumil <dhrumilcsk@gmail.com>
2022-03-07 15:14:32 -0500

    Removing unnecessary code from header.php file.

--- themes/projdeliverablesone/header.php
+++ themes/projdeliverablesone/header.php
@@ -89,29 +89,6 @@
 			endif;
 			?>
 			</div>
-			<div class="site-branding">
-				<?php
-				if ( ! empty( get_custom_logo() ) ) {
-					// the_custom_logo();
-				} else {
-					if ( is_front_page() && is_home() ) :
-						?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-					else :
-						?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php
-					endif;
-					$project__deliverables_1_description = get_bloginfo( 'description', 'display' );
-					if ( $project__deliverables_1_description || is_customize_preview() ) :
-						?>
-						<p class="site-description"><?php echo $project__deliverables_1_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-					<?php endif;
-				}
-				?>
-			</div>
-			<!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
 			<?php
