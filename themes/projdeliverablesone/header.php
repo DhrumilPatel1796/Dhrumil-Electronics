20f9041a3a26dc178e28ed743de3df078708ecf3
Dhrumil <dhrumilcsk@gmail.com>
2022-03-29 02:26:07 -0400

    Adding comments in the header.php file.

--- themes/projdeliverablesone/header.php
+++ themes/projdeliverablesone/header.php
@@ -23,37 +23,35 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			
-			<?php
-				the_custom_logo();
-
-				// <button>My Account</button>
-			?>
-			<!-- <img src="./assets/img/logo.png" /> -->
-			<?php
+	<header id="masthead" class="site-header"> <!-- giving id name masthead and class name site-header -->
+		<div class="site-branding"> <!-- creating div with class name site-branding -->
+			<div class="cell small-12 medium-4"><!-- creating div with class name cell and declaring the sizes for small adn medium screen -->
+			<!-- opening php tag -->	<?php 
+					the_custom_logo(); 
+				?><!-- closing php tag -->
+			</div> <!-- closing div -->
+			<!-- opening php tag and using if else condition --><?php
 			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="search">
+				?> <!-- closing php tag -->
+				<h1 class="search"> <!-- declaring h1 tag with class name search -->
 					<a href="
 						<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					</a>
-				</h1>
-				<?php
+					</a> <!-- opening a hyperlink tag and closing a hyperlink tag -->
+				</h1> <!-- closing h1 tag -->
+			<!-- opening php tag -->	<?php
 			else :
 				?>
-				<p class="site-search">
+				<p class="site-search"> <!-- opening p tag -->
 					<input type="search
-						<?php echo esc_url( home_url( '/' ) ); ?>" placeholder="Search" rel="home">
-				</p>
-				<?php
+						<?php echo esc_url( home_url( '/' ) ); ?>" placeholder="Search" rel="home"> <!-- using input tag for displaying search -->
+				</p> <!-- closing p tag -->
+				<!-- opening php tag and ending if else conditions --><?php
 			endif;
-			?>
-			</div>
+			?> <!-- ending php tag -->
+			</div> <!-- ending div  -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<?php
+		<nav id="site-navigation" class="main-navigation"> <!-- declaring nav with class name main-navigation and id name site-navigation -->
+			<!-- opening php tag and using wp_has_nav function for displaying primary menus and buttons --><?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-primary',
@@ -67,6 +65,6 @@
 					'menu_id'        => 'buttons-menu',
 				)
 			);
-			?>
+			?><!-- closing php tag -->
 		</nav> <!-- #site-navigation  -->
 	</header><!-- #masthead -->
